459dbc015a9b88fd68f57e16bceb3fb6309389ab
TTurvey <tturvey10@gmail.com>
2023-02-02 16:10:37 +0000

    GameOfLife should kill cell if it has zero neighbors

--- src/main/java/com/codurance/GameOfLife.java
+++ src/main/java/com/codurance/GameOfLife.java
@@ -1,4 +1,21 @@
 package com.codurance;
 
 public class GameOfLife {
+
+    private boolean[][] board;
+
+    public GameOfLife(boolean[][] board) {
+        this.board = board;
+    }
+
+    public void nextGen() {
+    }
+
+    public boolean[][] getBoard() {
+        return board;
+    }
+
+    public boolean cellStateAt(int i, int i1) {
+        return false;
+    }
 }
